3ee2ee56474e6d9f6166baf65b8d0de28b624515
martin <martin@cyberpower.kcl.ac.uk>
2011-08-31 12:31:11 +0100

    pyopengl

--- mma-lcos-clara/combine.py
+++ mma-lcos-clara/combine.py
@@ -3,6 +3,10 @@ from numpy import *
 import os
 import time
 import threading
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
 
 c=cdll.LoadLibrary("libcombine.so")
 
@@ -20,14 +24,13 @@ def capture():
 
 def init():
     c.init_clara()
-    os.environ['DISPLAY']=':0'
     c.init_mma()
     c.StartAcquisition()
-    c.init_lcos()
+    #c.init_lcos()
     
 def uninit():
     c.uninit_mma()
-    c.uninit_lcos()
+    #c.uninit_lcos()
     c.uninit_clara()
 
 def py():
@@ -62,3 +65,50 @@ def py_start():
 """
 #
 
+def initgl(w,h):
+    glClearColor(0,0,0,0)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0,w,h,0,-1,1)
+    glMatrixMode(GL_MODELVIEW)
+
+old=0
+count=0
+
+def drawfun():
+    global old, count
+    glClearColor(0,0,0,1)
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    glColor4d(1,1,1,1)
+    glTranslated(12,12,0)
+    #glScaled(dat.shape[1],dat.shape[2],1)
+    glScaled(100,100,1)
+    glColor3d(1,1,1,1)
+    #time.sleep(1/10.0)
+    glutSwapBuffers()
+    count=count+1
+    if 0==count%10: 
+        print 1/(time.time()-old)
+    old=time.time()
+    
+def draw():
+    drawfun()
+
+def main():
+    global window
+    glutInit("")
+    glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_ALPHA)
+    glutInitWindowSize(640,480)
+    window=glutCreateWindow("test")
+    glutDisplayFunc(draw)
+    glutIdleFunc(draw)
+    initgl(640,480)
+    glutMainLoop()
+
+app=threading.Thread(target=(lambda : main()))
+
+def run():
+    os.environ['DISPLAY']=':0'
+    os.environ['__GL_SYNC_TO_VBLANK']='1'
+    app.start()
